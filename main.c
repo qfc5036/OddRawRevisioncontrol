7f600b850cb6cd2a3ffa36ad3ef33ac6f1ff0077
qfc5036 <qfc5036@psu.edu>
2020-09-03 11:23:58 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,15 @@
+// Author Qiaoxu Cui qfc5036@psu.edu 
+// Collaborator Zhili Hu zjh5625@psu.edu
+// Collaborator Shufang Huang sbh5655@psu.edu 
+// Collaborator Isabel Vera ikv5018@psu.edu
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <readline/readline.h>
+int main(void) {
+  char *temperature =readline("Enter temperature in celsius: ");
+  double newtemp = atof(temperature)*1.8 +32;
+  double temp = atof(temperature);
+  printf("%f° in Celsius is equivenlent to %f° Fahrenheit.\n", temp, newtemp);
+  return 0;
+}
